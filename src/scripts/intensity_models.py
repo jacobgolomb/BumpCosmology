big updates, fixed jacobian, allow for different cosmologies in input and recovery

src/scripts/intensity_models.py
@@ -107,7 +107,139 @@ class LogDNDMPISN(object):
 
     def __call__(self, m):
         return jnp.interp(m, self.mbh_grid, self.log_dN_grid)
+
+@dataclass
+class LogDNDMPISN_evolve(object):
+    r"""
+    Function-like object representing the compact-object mass function induced
+    by the PISN process.
     
+    .. math::
+        \frac{\mathrm{d} N}{\mathrm{d} m} = \int \mathrm{d} m_\mathrm{CO} \, \frac{\mathrm{d} N}{\mathrm{d} m_\mathrm{CO}} p\left( m \mid \mu\left( m_\mathrm{CO} \mid m_\mathrm{PISN}, m_\mathrm{BH,max} \right), \sigma \right),
+
+    where the CO-remnant mass relationship is 
+
+    .. math::
+        \mu\left( m_\mathrm{CO} \mid m_\mathrm{PISN}, m_\mathrm{BH,max} \right) = \begin{cases}
+            m_\mathrm{CO} + 4 & m_\mathrm{CO} \leq m_\mathrm{PISN} - 4 \\
+            m_\mathrm{BH,max} - a \left( m_\mathrm{CO} - b \right^2 & \mathrm{otherwise}
+        \end{cases},
+
+    with :math:`a` and :math:`b` chosen so that the relation is smooth when
+    :math:`\mu = m_\mathrm{PISN}`; :math:`\sigma` is the amount of scatter
+    around the mean CO-remnant mass relationship, and the distribution of
+    offsets represented by :math:`p` is Gaussian.  
+
+    Parameters `a` and `b` are passed to :func:`log_dNdmCO`.
+
+    The (optional) `n_m` parameter controls how many points are used in the CO
+    mass and compact object mass grids to perform the above integral.  The
+    default setting works well for PISN/max BH masses around 50 MSun and `sigma`
+    parameters larger than ~1 solar mass; if sigma decreases below that value,
+    more grid points are needed to resolve the narrow width of the CO
+    mass-remnant mass relation, and similarly if the peak mass scale increases.
+    """
+    a: object
+    b: object
+    mpisn: object
+    mbhmax: object
+    sigma: object
+    n_m: object = 256
+    mbh_grid: object = dataclasses.field(init=False)
+    log_dN_grid: object = dataclasses.field(init=False)
+    
+    
+    
+    #def __post_init__(self):
+        
+
+    def __post_init__(self):
+        min_bh_mass = 3.0
+        min_co_mass = 1.0
+        max_bh_mass = jnp.max(self.mbhmax + 7*self.sigma)
+        max_co_mass = largest_mco(self.mpisn, self.mbhmax)
+
+        mbh = jnp.linspace(min_bh_mass, max_bh_mass, self.n_m)
+        mco = jnp.linspace(min_co_mass, max_co_mass, self.n_m)
+                
+        log_wts = log_dNdmCO(mco[None,:], self.a, self.b) - 0.5*jnp.square((mbh[:,None,None] - mean_mbh_from_mco(mco[None,:], self.mpisn, self.mbhmax))/self.sigma) - np.log(np.sqrt(2*np.pi)) - jnp.log(self.sigma)        
+        log_trapz = np.log(0.5) + jnp.logaddexp(log_wts[:,1:, :], log_wts[:,:-1, :]) + jnp.log(jnp.diff(mco[None,:], axis=1))
+        self.log_dN_grid = jss.logsumexp(log_trapz, axis=1)
+        self.mbh_grid = mbh
+
+    def __call__(self, m):
+        return jnp.interp(m, self.mbh_grid, self.log_dN_grid)
+
+
+@dataclass
+class LogDNDM_evolve(object):
+    """
+    Function-like object representing the full compact object mass function.
+
+    A combination of :class:`LogDNDMPISN` above and a power law that begins at
+    :math:`m = m_\mathrm{BH,max}` with relative amplitude `fpl` and slope `c`.
+    """
+    a: object
+    b: object
+    c: object
+    mpisn: object
+    mpisndot: object
+    mbhmax: object
+    sigma: object
+    fpl: object
+    mref: object = 30.0
+    zref: object = 0
+    log_norm: object = 0.0
+    log_pl_norm: object = dataclasses.field(init=False)
+    log_dndm_pisn: object = dataclasses.field(init=False)
+
+    def __post_init__(self):
+        self.setup_interp()
+        self.log_pl_norm = jnp.log(self.fpl) + self.interp_2d(self.mbhmax, self.zref)
+
+        self.log_norm = -(self(np.array(self.mref), self.zref) + jnp.log(self.mref)) # normalize so that m dNdm = 1 at mref
+
+    def setup_interp(self):
+        self.z_array = jnp.expm1(np.linspace(np.log(1), np.log(1+10), 20))
+        mpisns = self.mpisn + self.mpisndot * (1 - 1/(1+self.z_array))
+        self.log_dndm_pisn = LogDNDMPISN_evolve(self.a, self.b, np.array(mpisns), self.mbhmax, self.sigma)
+        self.mbh_grid = self.log_dndm_pisn.mbh_grid
+        self.log_dndm_pisn_grid = self.log_dndm_pisn.log_dN_grid
+    
+    def interp_2d(self, m, z):
+        m_indxs = jnp.searchsorted(self.mbh_grid, m)
+        z_indxs = jnp.searchsorted(self.z_array, z)
+        
+        m_upper = self.mbh_grid[m_indxs]
+        m_lower = self.mbh_grid[m_indxs - 1]
+        
+        z_upper = self.z_array[z_indxs]
+        z_lower = self.z_array[z_indxs - 1]
+        
+        f1 = self.log_dndm_pisn_grid[m_indxs - 1, z_indxs - 1]
+        f2 = self.log_dndm_pisn_grid[m_indxs, z_indxs - 1]
+        f3 = self.log_dndm_pisn_grid[m_indxs, z_indxs]
+        f4 = self.log_dndm_pisn_grid[m_indxs - 1, z_indxs]
+        
+        t = (m - m_lower) / (m_upper - m_lower)
+        u = (z - z_lower) / (z_upper - z_lower)
+        
+        return (1 - t) * (1 - u) * f1 + t * (1 - u) * f2 + t * u *f3 + (1 - t) * u * f4
+        
+        
+        
+    def __call__(self, m, z):
+        m = jnp.array(m)
+        z = jnp.array(z) 
+        log_dNdm = self.interp_2d(m, z)
+
+        log_dNdm = jnp.where(m <= np.min(self.mbh_grid[0]), np.NINF, log_dNdm)
+        log_dNdm = jnp.where(m >= np.max(self.mbh_grid[-1]), np.NINF, log_dNdm)
+
+        log_dNdm = jnp.logaddexp(log_dNdm, -self.c*jnp.log(m/self.mbhmax) + self.log_pl_norm + log_smooth_turnon(m, self.mbhmax))
+
+        return log_dNdm + self.log_norm
+
 @dataclass
 class LogDNDM(object):
     """
@@ -133,6 +265,7 @@ class LogDNDM(object):
         self.log_pl_norm = jnp.log(self.fpl) + self.log_dndm_pisn(self.mbhmax)
 
         self.log_norm = -(self(np.array(self.mref)) + jnp.log(self.mref)) # normalize so that m dNdm = 1 at mref
+
 
     def __call__(self, m):
         m = jnp.array(m)
@@ -205,6 +338,43 @@ class LogDNDMDQDV(object):
         return self.log_dndm(m1) + self.log_dndm(m2) + self.beta*jnp.log(mt/(self.mref*(1 + self.qref))) + jnp.log(m1) + self.log_dndv(z)
 
 @dataclass
+class LogDNDMDQDV_evolve(object):
+    r"""
+    TODO: Document pairing function, arguments.
+    """
+    a: object
+    b: object
+    c: object
+    mpisn: object
+    mpisndot: object
+    mbhmax: object
+    sigma: object
+    fpl: object
+    beta: object
+    lam: object
+    kappa: object
+    zp: object
+    mref: object = 30.0
+    qref: object = 1.0
+    zref: object = 0.0
+    log_dndm: object = dataclasses.field(init=False)
+    log_dndv: object = dataclasses.field(init=False)
+
+
+    def __post_init__(self):
+        self.log_dndm = LogDNDM_evolve(self.a, self.b, self.c, self.mpisn, self.mpisndot, self.mbhmax, self.sigma, self.fpl, self.mref)
+        self.log_dndv = LogDNDV(self.lam, self.kappa, self.zp, self.zref)
+
+    def __call__(self, m1, q, z):
+        m1 = jnp.array(m1)
+        q = jnp.array(q)
+        z = jnp.array(z)
+
+        m2 = q*m1
+        mt = m1+m2
+
+        return self.log_dndm(m1) + self.log_dndm(m2) + self.beta*jnp.log(mt/(self.mref*(1 + self.qref))) + jnp.log(m1) + self.log_dndv(z)
+@dataclass
 class FlatwCDMCosmology(object):
     """
     Function-like object representing a flat w-CDM cosmology.
@@ -212,7 +382,7 @@ class FlatwCDMCosmology(object):
     h: object
     Om: object
     w: object
-    zmax: object = 100.0
+    zmax: object = 20.0
     ninterp: object = 1024
     zinterp: object = dataclasses.field(init=False)
     dcinterp: object = dataclasses.field(init=False)
@@ -284,7 +454,7 @@ def pop_model(m1s, qs, zs, pdraw, m1s_sel, qs_sel, zs_sel, pdraw_sel, Ndraw):
     log_pdraw = jnp.log(pdraw)
     log_pdraw_sel = jnp.log(pdraw_sel)
 
-    zmax = 100
+    zmax = 20
     zinterp = np.expm1(np.linspace(np.log1p(0), np.log1p(zmax), 1024))
     dVdzdt_interp = 4*np.pi*Planck18.differential_comoving_volume(zinterp).to(u.Gpc**3/u.sr).value/(1+zinterp)
     
@@ -332,7 +502,7 @@ def pop_model(m1s, qs, zs, pdraw, m1s_sel, qs_sel, zs_sel, pdraw_sel, Ndraw):
     _ = numpyro.deterministic('dNdqdVdt_fixed_mz', log_dN.mref*R*jnp.exp(log_dN(log_dN.mref, coords['q_grid'], log_dN.zref)))
     _ = numpyro.deterministic('dNdVdt_fixed_mq', log_dN.mref*R*jnp.exp(log_dN(log_dN.mref, log_dN.qref, coords['z_grid'])))
 
-def pop_cosmo_model(m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel, Ndraw):
+def pop_cosmo_model(m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel, Ndraw, evolution = False):
     m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel = map(jnp.array, (m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw_sel))
 
     nobs = m1s_det.shape[0]
@@ -351,7 +521,7 @@ def pop_cosmo_model(m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw
     b = numpyro.sample('b', dist.Normal(1.9, 2))
     c = numpyro.sample('c', dist.Normal(4, 2))
 
-    mpisn = numpyro.sample('mpisn', dist.TruncatedNormal(35.0, 5.0, low=0))
+    mpisn = numpyro.sample('mpisn', dist.TruncatedNormal(35.0, 5.0, low=15))
     dmbhmax = numpyro.sample('dmbhmax', dist.TruncatedNormal(5.0, 2.0, low=0.0))
     mbhmax = numpyro.deterministic('mbhmax', mpisn + dmbhmax)
     sigma = numpyro.sample('sigma', dist.TruncatedNormal(2, 2, low=1))
@@ -368,7 +538,11 @@ def pop_cosmo_model(m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw
 
     cosmo = FlatwCDMCosmology(h, Om, w)
 
-    log_dN = LogDNDMDQDV(a, b, c, mpisn, mbhmax, sigma, fpl, beta, lam, kappa, zp)
+    if not evolve:
+        log_dN = LogDNDMDQDV(a, b, c, mpisn, mbhmax, sigma, fpl, beta, lam, kappa, zp)
+    else:
+        mpisndot = numpyro.sample('mpisndot', dist.TruncatedNormal(4, 2, low=0) )
+        log_dN = LogDNDMDQDV_evolve(a, b, c, mpisn, mbhmax, mpisndot, sigma, fpl, beta, lam, kappa, zp)
 
     zs = cosmo.z_of_dL(dls)
     m1s = m1s_det / (1 + zs)
@@ -399,3 +573,102 @@ def pop_cosmo_model(m1s_det, qs, dls, pdraw, m1s_det_sel, qs_sel, dls_sel, pdraw
     _ = numpyro.deterministic('dNdqdVdt_fixed_mz', log_dN.mref*R*jnp.exp(log_dN(log_dN.mref, coords['q_grid'], log_dN.zref)))
     _ = numpyro.deterministic('dNdVdt_fixed_mq', log_dN.mref*R*jnp.exp(log_dN(log_dN.mref, log_dN.qref, coords['z_grid'])))
     _ = numpyro.deterministic('hz', cosmo.h*cosmo.E(coords['z_grid']))
+
+def pop_cosmo_model_noselection(m1s_det, qs, dls, pdraw, **kwargs):
+    m1s_det, qs, dls, pdraw = map(jnp.array, (m1s_det, qs, dls, pdraw))
+
+    nobs = m1s_det.shape[0]
+    nsamp = m1s_det.shape[1]
+
+    #log_pdraw = jnp.log(pdraw)
+
+    h = numpyro.sample('h', dist.TruncatedNormal(0.7, 0.2, low=0.35, high=1.4))
+    Om = numpyro.sample('Om', dist.TruncatedNormal(0.3, 0.15, low=0, high=1))
+    w = numpyro.sample('w', dist.TruncatedNormal(-1, 0.25, low=-1.5, high=-0.5))
+
+    a = numpyro.sample('a', dist.Normal(2.35, 2))
+    b = numpyro.sample('b', dist.Normal(1.9, 2))
+    c = numpyro.sample('c', dist.Normal(4, 2))
+
+    mpisn = numpyro.sample('mpisn', dist.TruncatedNormal(35.0, 5.0, low=15))
+    dmbhmax = numpyro.sample('dmbhmax', dist.TruncatedNormal(5.0, 2.0, low=0.0))
+    mbhmax = numpyro.deterministic('mbhmax', mpisn + dmbhmax)
+    sigma = numpyro.sample('sigma', dist.TruncatedNormal(2, 2, low=1))
+
+    beta = numpyro.sample('beta', dist.Normal(0, 2))
+
+    log_fpl = numpyro.sample('log_fpl', dist.Uniform(np.log(1e-3), np.log(0.5)))
+    fpl = numpyro.deterministic('fpl', jnp.exp(log_fpl))
+    
+    lam = numpyro.sample('lam', dist.Normal(2.7, 2.0))
+    dkappa = numpyro.sample('dkappa', dist.TruncatedNormal(5.6-2.7, 2.0, low=1))
+    kappa = numpyro.deterministic('kappa', lam + dkappa)
+    zp = numpyro.sample('zp', dist.TruncatedNormal(1.9, 1, low=0))
+    """
+    params_dict = dict(
+            a= a,
+            b =b,
+            c = c,
+            mpisn= mpisn,
+            mbhmax = mbhmax,
+            fpl = fpl,
+    beta = beta,
+    lam = lam,
+    kappa = kappa,
+    zp = zp,
+    h = h,
+    Om = Om,
+    w = w)
+    """
+
+    cosmo = FlatwCDMCosmology(h, Om, w)
+    zs = cosmo.z_of_dL(dls)
+    m1s = m1s_det / (1 + zs)
+    """"
+    log_dN = LogDNDMDQDV(a, b, c, mpisn, mbhmax, sigma, fpl, beta, lam, kappa, zp)
+
+
+    m1_line = jnp.linspace(5, 150, 300)
+    q_line = jnp.linspace(0.01, 1, 200)
+    mm, qq = jnp.meshgrid(m1_line, q_line)
+
+    mass_norm_grid =  jnp.exp(log_dN(mm, qq, z=0.001))
+    mass_norm = jnp.trapz(x = q_line, y = jnp.trapz(x = m1_line, y = mass_norm_grid))
+
+    z_norm_grid = jnp.exp(log_dN(m1=30, q=1, z = cosmo.zinterp))
+    z_norm = jnp.trapz(x = cosmo.zinterp, y = z_norm_grid)
+
+    total_norm = mass_norm * z_norm
+
+    log_wts = log_dN(m1s, qs, zs) - jnp.log(total_norm) - 2*jnp.log1p(zs) + jnp.log(cosmo.dVCdz(zs)) - jnp.log(cosmo.ddL_dz(zs)) #- log_pdraw
+    """
+    log_dN = LogDNDMDQDV(a, b, c, mpisn, mbhmax, sigma, fpl, beta, lam, kappa, zp)
+    #log_dN = log_dN_func(m1s, qs, zs)
+
+    log_p_unnormed = log_dN(m1s, qs, zs) + jnp.log(cosmo.dVCdz(zs)) - jnp.log1p(zs)
+    m1_line = jnp.linspace(5, 150, 300)
+    q_line = jnp.linspace(0.01, 1, 200)
+    mm, qq = jnp.meshgrid(m1_line, q_line)
+
+    mass_norm_grid =  jnp.exp(log_dN(mm, qq, z=0.001))
+    mass_norm = jnp.trapz(x = q_line, y = jnp.trapz(x = m1_line, y = mass_norm_grid))
+
+    z_norm_grid = jnp.exp(log_dN(m1=30, q=1, z = cosmo.zinterp)) *cosmo.dvcinterp/(1+cosmo.zinterp)
+    z_norm = jnp.trapz(x = cosmo.zinterp, y = z_norm_grid)
+
+    total_norm = mass_norm * z_norm
+    log_wts  = log_p_unnormed - jnp.log(total_norm) - jnp.log1p(zs) - jnp.log(cosmo.ddL_dz(zs))
+    log_like = jss.logsumexp(log_wts, axis=1) - jnp.log(nsamp)
+    _ = numpyro.factor('loglike', jnp.sum(log_like))
+
+    #VT =  jnp.trapz(x = cosmo.zinterp, y=cosmo.dVCdz(cosmo.zinterp) / (1 + cosmo.zinterp))
+    R = numpyro.sample('Ntotal', dist.Normal(nobs, jnp.sqrt(nobs)))
+    #R = numpyro.deterministic('R_density', Ntotal*jnp.exp(log_dN(m,q,z))/total_norm)
+
+    _ = numpyro.deterministic('neff', jnp.exp(2*jss.logsumexp(log_wts, axis=1) - jss.logsumexp(2*log_wts, axis=1)))
+    """
+    _ = numpyro.deterministic('mdNdmdVdt_fixed_qz', coords['m_grid']*R*jnp.exp(log_dN(coords['m_grid'], log_dN.qref, log_dN.zref)))
+    _ = numpyro.deterministic('dNdqdVdt_fixed_mz', log_dN.mref*R*jnp.exp(log_dN(log_dN.mref, coords['q_grid'], log_dN.zref)))
+    _ = numpyro.deterministic('dNdVdt_fixed_mq', log_dN.mref*R*jnp.exp(log_dN(log_dN.mref, log_dN.qref, coords['z_grid'])))
+    _ = numpyro.deterministic('hz', cosmo.h*cosmo.E(coords['z_grid']))
+    """

src/scripts/mock_injections.py
@@ -4,10 +4,12 @@ import lal
 import lalsimulation as ls
 import numpy as np
 import os.path as op
+import sys
 import pandas as pd
 import paths
 from tqdm import tqdm
 import weighting
+import intensity_models
 
 def next_pow_2(x):
     np2 = 1
@@ -30,8 +32,7 @@ def compute_snrs(d):
 
             a1 = np.sqrt(r.s1x*r.s1x + r.s1y*r.s1y + r.s1z*r.s1z)
             a2 = np.sqrt(r.s2x*r.s2x + r.s2y*r.s2y + r.s2z*r.s2z)
-
-            dl = Planck18.luminosity_distance(r.z).to(u.Gpc).value * 1e9*lal.PC_SI
+            dl = d['dL'] * 1e9*lal.PC_SI
 
             fmin = 9.0
             fref = fmin
@@ -84,19 +85,69 @@ def compute_snrs(d):
 ndraw = 100000000
 nbatch = 10000
 
+def calc_nex(df_det, default_settings, **kwargs):
+    for key in df_det.keys():
+        df_det[key] = np.array(df_det[key])
+    if default_settings:
+        h, Om, w = Planck18.h, Planck18.Om0, -1
+        log_dN_func = weighting.default_log_dNdmdqdV
+        rate = weighting.default_parameters.R
+    else:
+        pop_params = {key: kwargs[key] for key in ModelParameters().keys()}
+        h, Om, w = kwargs['h'], kwargs['Om'], kwargs['w']
+        rate = kwargs['R']
+        log_dN_func = intensity_models.LogDNDMDQDV(**pop_params)
+    if "cosmo" not in kwargs.keys():
+        cosmo = intensity_models.FlatwCDMCosmology(h, Om, w)
+    else:
+        cosmo = kwargs.get("cosmo")
+    log_dN = log_dN_func(df_det['m1'], df_det['q'], df_det['z'])
+    nex = np.sum(rate*np.exp(log_dN)*cosmo.dVCdz(df_det['z'])*4*np.pi/(1+df_det['z'])/df_det['pdraw_mqz'])/len(df)
+    return nex
+
+
+
 if __name__ == '__main__':
+
+    if sys.argv[1] == 'default':
+        default = True
+    else:
+        default = False
+        custom_params = sys.argv[1]
+    if sys.argv[2]:
+        snr_threshold = float(sys.argv[2])
+    else:
+        snr_threshold = 0
+    if sys.argv[3]:
+        outfile = sys.argv[3]
+    else:
+        outfile = op.join(paths.data, 'mock_injections.h5')
+
+    population_parameters = dict()
+    if not default:
+        with open("file.txt") as param_file:
+            for line in param_file:
+                (key, val) = line.split('=')
+                population_parameters[key.strip()] = float(val.strip())
+
+    custom_cosmo = None
+    if not default:
+        custom_cosmo = intensity_models.FlatwCDMCosmology(population_parameters['h'], population_parameters['Om'], population_parameters['w'])
+        population_parameters['cosmo'] = custom_cosmo
     rng = np.random.default_rng(333165393797366967556667466879860422123)
 
     wt_sum = 0
 
     with tqdm(total=ndraw) as bar:
-        df = pd.DataFrame(columns = ['m1', 'q', 'z', 'iota', 'ra', 'dec', 'psi', 'gmst', 's1x', 's1y', 's1z', 's2x', 's2y', 's2z', 'pdraw_mqz', 'SNR_H1', 'SNR_L1', 'SNR_V1', 'SNR'])
+        df = pd.DataFrame()
+        #df = pd.DataFrame(columns = ['m1', 'q', 'z', 'iota', 'ra', 'dec', 'psi', 'gmst', 's1x', 's1y', 's1z', 's2x', 's2y', 's2z', 'pdraw_mqz', 'SNR_H1', 'SNR_L1', 'SNR_V1', 'SNR'])
         for _ in range(ndraw // nbatch):
 
             m = rng.uniform(low=5, high=150, size=nbatch)
             q = rng.uniform(low=0, high=1, size=nbatch)
-            z = rng.uniform(low=0, high=2.75, size=nbatch)
+            z = rng.uniform(low=0, high=20, size=nbatch)
 
+            m1d = m * (1 + z)
             iota = np.arccos(rng.uniform(low=-1, high=1, size=nbatch))
 
             ra = rng.uniform(low=0, high=2*np.pi, size=nbatch)
@@ -109,9 +160,15 @@ if __name__ == '__main__':
             s1x, s1y, s1z = rng.normal(loc=0, scale=0.2/np.sqrt(3), size=(3, nbatch))
             s2x, s2y, s2z = rng.normal(loc=0, scale=0.2/np.sqrt(3), size=(3, nbatch))
 
-            pdraw = 1/(150-5)*1*1/2.75
+            pdraw = 1/(150-5)*1*1/20
+            if default:
+                dL = Planck18.luminosity_distance(z).to(u.Gpc).value
+                dm1sz_dm1ddl = weighting.dm1sz_dm1ddl(z, cosmo=None)
+            else:
+                dm1sz_dm1ddl = weighting.dm1sz_dm1ddl(z, cosmo=population_parameters['cosmo'])
+                dL = population_parameters['cosmo'].dL(z)
 
-            wt = weighting.default_pop_wt(m, q, z)
+            wt = np.array(weighting.pop_wt(m, q, z, default=default, **population_parameters))
 
             wt[m*q < 5] = 0 # Cut out events with m2 < 5
             wt_sum += np.sum(wt/pdraw)
@@ -123,6 +180,8 @@ if __name__ == '__main__':
                 'm1': m[sel],
                 'q': q[sel],
                 'z': z[sel],
+                'dL': dL[sel],
+                'm1d': m1d[sel],
                 'iota': iota[sel],
                 'ra': ra[sel],
                 'dec': dec[sel],
@@ -134,20 +193,23 @@ if __name__ == '__main__':
                 's2x': s2x[sel],
                 's2y': s2y[sel],
                 's2z': s2z[sel],
-                'pdraw_mqz': wt[sel]
+                'pdraw_mqz': wt[sel],
+                'dm1sz_dm1ddl': dm1sz_dm1ddl[sel]
             })
-            d = compute_snrs(d)
-
+            if snr_threshold > 0:
+                d = compute_snrs(d)
+            else:
+                d['SNR'] = 10000000
             df = pd.concat((df, d))
             bar.update(nbatch)
 
     wt_sum /= ndraw
     df['pdraw_mqz'] = df['pdraw_mqz'] / wt_sum
 
-    df.to_hdf(op.join(paths.data, 'mock_injections.h5'), key='true_parameters')
+    df.to_hdf(outfile, key='true_parameters')
 
-    df_det = df[df['SNR'] > 9]
-    nex = np.sum(weighting.default_parameters.R*np.exp(weighting.default_log_dNdmdqdV(df_det['m1'], df_det['q'], df_det['z']))*Planck18.differential_comoving_volume(df_det['z']).to(u.Gpc**3/u.sr).value*4*np.pi/(1+df_det['z'])/df_det['pdraw_mqz'])/len(df)
-
-    print('Found {:d} injections with SNR > 9'.format(np.sum(df['SNR'] > 9)))
+    df_det = df[df['SNR'] > snr_threshold]
+    #nex = np.sum(weighting.default_parameters.R*np.exp(weighting.default_log_dNdmdqdV(df_det['m1'], df_det['q'], df_det['z']))*Planck18.differential_comoving_volume(df_det['z']).to(u.Gpc**3/u.sr).value*4*np.pi/(1+df_det['z'])/df_det['pdraw_mqz'])/len(df)
+    nex = calc_nex(df_det, default_settings = default, **population_parameters)
+    print('Found {:d} injections with SNR > {:d}'.format(np.sum(df['SNR'] > snr_threshold), snr_threshold))
     print('Predicting {:.0f} detections per year'.format(nex))

src/scripts/weighting.py
@@ -7,7 +7,9 @@ import h5py
 import intensity_models
 import jax.numpy as jnp
 import numpy as np
+import intensity_models
 
+COSMO_PARAMS = ['h', 'w', 'Om']
 @dataclass
 class ModelParameters(object):
     a: object = 1.2
@@ -34,6 +36,21 @@ def default_pop_wt(m1, q, z):
     """Weights in `(m1,q,z)` corresponding to the :func:`default_log_dNdmdqdV`."""
     log_dN = default_log_dNdmdqdV(m1, q, z)
     return np.exp(log_dN)*Planck18.differential_comoving_volume(z).to(u.Gpc**3/u.sr).value/(1+z)
+
+def pop_wt(m1, q, z, default=True, **kwargs):
+    if default:
+        h, Om, w = Planck18.h, Planck18.Om0, -1
+        log_dN_func = default_log_dNdmdqdV
+    else:
+        pop_params = {key: kwargs[key] for key in ModelParameters().keys()}
+        h, Om, w = kwargs['h'], kwargs['Om'], kwargs['w']
+        log_dN_func = intensity_models.LogDNDMDQDV(**pop_params)
+    if "cosmo" not in kwargs.keys():
+        cosmo = intensity_models.FlatwCDMCosmology(h, Om, w)
+    else:
+        cosmo = kwargs.get("cosmo")
+    log_dN = log_dN_func(m1, q, z)
+    return np.exp(log_dN) * cosmo.dVCdz(z) / (1+z)
 
 def li_prior_wt(m1, q, z, cosmology_weighted=False):
     """Returns LALInference/Bilby prior over `m1`, `q`, and `z`.
@@ -166,8 +183,15 @@ def extract_selection_samples(file, nsamp, desired_pop_wt=None, far_threshold=1,
 
         return m1s_sel_cut, qs_sel_cut, zs_sel_cut, pdraw_sel_cut, ndraw_cut
     
-def dm1qz_dm1dqdl(m1, q, z):
-    return (1+z)/(Planck18.comoving_distance(z).to(u.Gpc).value + (1+z)*Planck18.hubble_distance.to(u.Gpc).value / Planck18.efunc(z))
+def dm1sz_dm1ddl(z, cosmo=None):
+    if not cosmo:
+        #return (1+z) / (Planck18.comoving_distance(z).to(u.Gpc).value + (1+z)*Planck18.hubble_distance.to(u.Gpc).value / Planck18.efunc(z))
+        dm1s_dm1d = (1+z)**-1
+        ddl_dz = (Planck18.comoving_distance(z).to(u.Gpc).value + (1 + z) * Planck18.hubble_distance.to(u.Gpc).value / Planck18.efunc(z))
+
+        return dm1s_dm1d * (ddl_dz)**-1
+    else:
+        return (1+z)**-1 / (cosmo.dC(z) + (1+z)*cosmo.dH / cosmo.E(z))
 
 def draw_mock_samples(log_mc_obs, sigma_log_mc, q_obs, sigma_q, log_dl_obs, sigma_log_dl, size=1, output_source_frame=False, rng=None):
     if rng is None:
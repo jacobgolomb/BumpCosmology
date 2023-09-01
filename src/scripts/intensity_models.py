fixed broadcasting

src/scripts/intensity_models.py
@@ -169,13 +169,13 @@ class LogDNDMPISN_evolve(object):
         max_bh_mass = jnp.max(self.mbhmax *(1 +7*self.sigma))
         max_co_mass = largest_mco(self.mpisn, self.mbhmax)
 
-        mbh = jnp.linspace(min_bh_mass, max_bh_mass, self.n_m)
+        mbh = jnp.linspace(min_bh_mass, max_bh_mass, self.n_m+2)
         mco = jnp.linspace(min_co_mass, max_co_mass, self.n_m)
 
         sigma = sigma_mbh_from_mco(mco[None,:], self.mpisn, self.mbhmax, self.sigma)
         mu = mean_mbh_from_mco(mco[None,:], self.mpisn, self.mbhmax)
 
-        log_wts = log_dNdmCO(mco[None,:], self.a, self.b) - 0.5*jnp.square((jnp.log(mbh[:,None,None]) - jnp.log(mu))/sigma) - np.log(np.sqrt(2*np.pi)) - jnp.log(sigma) - jnp.log(mbh[:,None])     
+        log_wts = log_dNdmCO(mco[None,:], self.a, self.b) - 0.5*jnp.square((jnp.log(mbh[:,None,None]) - jnp.log(mu))/sigma) - np.log(np.sqrt(2*np.pi)) - jnp.log(sigma) - jnp.log(mbh[:,None,None])     
         log_wts = jnp.nan_to_num(log_wts, nan = -jnp.inf)
         log_trapz = np.log(0.5) + jnp.logaddexp(log_wts[:,1:, :], log_wts[:,:-1, :]) + jnp.log(jnp.diff(mco[None,:], axis=1))
         self.log_dN_grid = jss.logsumexp(log_trapz, axis=1)